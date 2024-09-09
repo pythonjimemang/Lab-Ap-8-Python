336efacaa2376626598b396c76129fb2310e60fc
unknown <akmalrbc6@gmail.com>
2024-09-09 20:52:37 +0800

    Kumpul Tugas 1

---
+++ H071241065 - Akmal/no4.py
@@ -0,0 +1,9 @@
+
+
+suhuDerajat = float(input("Masukkan suhu dalm celcius : "))
+
+Kelvin = suhuDerajat + 273
+Reamur = suhuDerajat * 4 // 5
+Farenheit = (suhuDerajat * 9//5) + 32
+
+print(f"Hasil konversi dari suhu {suhuDerajat} derajat ke kelvin adalah : {Kelvin}K \nHasil konversi dari suhu {suhuDerajat} deerajat ke reamur adalah : {Reamur}R \nHasil konversi dari suhu {suhuDerajat} derajat ke farenheit adalah : {Farenheit}F ")
